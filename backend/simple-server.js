9eea892d4d3cb5f49c92b1129a7a5136eaf523ed
Squid <Squid@squidtoken.org>
2025-09-27 18:39:07 -0400

    fix: add Vercel domain to CORS configuration for frontend-backend communication

--- backend/simple-server.js
+++ backend/simple-server.js
@@ -83,7 +83,9 @@ app.use(cors({
     'http://localhost:3000', 
     'http://127.0.0.1:3000',
     'https://launchonlos.fun',
-    'https://www.launchonlos.fun'
+    'https://www.launchonlos.fun',
+    'https://analos-nft-launcher-uz4a.vercel.app',
+    'https://*.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
